e9b65e314c18436ac2002d2708bd5050d5fe31f3
Yuri Dirickson <yuri.dirickson@usp.br>
2020-10-27 15:17:09 -0300

    Aula08 SSR

--- controllers.py
+++ controllers.py
@@ -1,4 +1,5 @@
 from app import app
+from database.funcoes import obter_curso_por_sigla, listar_disciplinas_por_sigla
 from flask import render_template
 
 
@@ -10,3 +11,24 @@ def index():
 @app.route('/sobre')
 def sobre():
     return render_template('sobre.html')
+
+
+@app.route('/contato')
+def contato():
+    return render_template('contato.html')
+
+
+@app.route('/entrar')
+def entrar():
+    return render_template('entrar.html')
+
+
+@app.route('/cursos/ads')
+def curso_ads():
+    curso_ads = obter_curso_por_sigla('ads')
+    disciplinas = listar_disciplinas_por_sigla('ads')
+    return render_template(
+        'curso.html',
+        curso=curso_ads,
+        disciplinas=disciplinas
+    )
